bdefd4c2f363dcda694237e5b4d1342ce0305b6d
Duk000 <duk000@hotmail.com>
2023-09-12 16:39:56 -0500

    first commit

---
+++ app/src/main/java/com/hotelalura/app/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.hotelalura.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+
+        System.out.println("Bievenido al Login");
+        return "login";
+    }
+
+}
